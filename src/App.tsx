b353740b140f9ed43030cf7c561534d0aaab7749
Lincoln Dias Marques <licodmarques@gmail.com>
2022-10-07 04:58:10 -0300

    tailwind, router com rotas e layout

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+import "./styles/globals.css";
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
+}
