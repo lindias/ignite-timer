cba2dc99480990ae2406a4f4f36732550c10c13e
Lincoln Dias Marques <licodmarques@gmail.com>
2023-01-16 16:36:58 -0300

    fix history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -38,7 +38,7 @@ export function History() {
                     {cycle.minutesAmount} minutos
                   </td>
                   <td className="bg-gray-700 border-t-4 border-gray-800 p-4 text-sm leading-relaxed">
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
